9fafd8591a0cfb778c80418d6c1bb3162b2ce2b6
ahmed <newl4533@gmail.com>
2025-02-22 14:26:54 +0200

    last

--- finance_project/urls.py
+++ finance_project/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('tracker.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
     path('accounts/', include('allauth.urls')),
 ]
